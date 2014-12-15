30f945a8b559e2d1655b7eb380d22ae49e2e2dea
Brett Merriam <bmerriam89@gmail.com>
2014-12-14 20:39:14 -0600

    Code for iOS mobile app thus far
    All code that I have completed for the iOS app. Still a work in
    progress.

---
+++ ToDoList/ToDoList/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  ToDoList
+//
+//  Created by Brett Merriam on 12/11/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
---
+++ ToDoList/ToDoList/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Brett Merriam on 12/10/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
