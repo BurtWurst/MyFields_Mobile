b1e5b81b902e198903c0fb788bb5b8e68809ae88
Daniel G. Kennedy <dgken2015@gmail.com>
2015-02-03 22:18:59 -0600

    Added basic code for connecting to the database

---
+++ API/DBConnect.php
@@ -0,0 +1,40 @@
+<?php
+	
+	$dbHost = "mysql.cis.ksu.edu";
+	$dbName = "proj_myfields";
+	
+	$userName = "proj_myfields";
+	$password = "----";
+	
+	$db = new PDO('mysql:host=' . $dbHost . ';dbname=' . $dbName . ';charset=utf8', 
+				   $userName, 
+				   $password);
+					
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+	
+	
+	try
+	{
+		$field;
+		
+		foreach($db->query("select * from FIELDS where Owner = '" . $user->uid . "'" as $row)
+		{
+			$field = new Field('stuff');
+			
+			foreach($db->query("select * from PESTSAMPLES where FieldId = '" . $field->getID() . "'" as $sample)
+			{
+				$field->addSample(new PestSample('stuff'));
+			}
+			
+			echo $field->JSONize();
+		}
+	}
+	catch(PDOException $problem)
+	{
+		echo 'Problem connecting to database: ' . $problem;
+	}
+?>
+
+{ 'id' = 1;
+'name' = 'Field5' }
