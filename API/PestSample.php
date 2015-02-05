e4a2e27fe65d44f0df7c532b9a4b52f50352841a
Daniel G. Kennedy <dgken2015@gmail.com>
2015-02-04 21:33:35 -0600

    Refactored PestSample to build from database info instead of random info and added JSON formatting to its output

--- API/PestSample.php
+++ API/PestSample.php
@@ -3,28 +3,78 @@
 class PestSample 
 {
 	
-	private static $SampleNumber = 1;			
+	protected $Id;			
 	
-	private $Location;
-	private $Site;
-	private $ControlCost;
-	private $CropValue;	
+	protected $Location;
+	protected $Field;
+	protected $ControlCost;
+	protected $CropValue;	
 	
-	private $Notes;
-	private $OtherPests;
+	protected $Notes;
+	protected $OtherPests;
 	
 	
-	public function __construct()
+	public function __construct($Identifier, $Latitude, $Longitude, $Site, $TreatmentCost, $CropCost, $NoteSection, $Pests)
 	{
+		$Id = $Identifier;
+		$Location = new GPSLocation($Latitude, $Longitude);
+		$Field = $Site;
+		$ControlCost = $TreatmentCost;
+		$CropValue = $CropCost;
 		
+		$Notes = $NoteSection;
+		$OtherPests = $Pests;
 		
 	}
 	
 	public function JSONize()
 	{
-		return json_encode(get_object_vars($this));
+		throw new Exception("Not Implemented!");
 	}
 }
 
+class GreenbugSample extends PestSample
+{
+	protected $AphidCount;
+	protected $MummyCount;
+	
+	public function __construct($Identifier, $Latitude, $Longitude, $Site, $TreatmentCost, $CropCost, $NoteSection, $Pests, $Aphids, $Mummys)
+	{
+		parent::__construct($Identifier, $Latitude, $Longitude, $Site, $TreatmentCost, $CropCost, $NoteSection, $Pests);
+		
+		$AphidCount = $Aphids;
+		$MummyCount = $Mummys;
+	}
+	
+	public function JSONize()
+	{
+		echo '\t{';
+		echo '\t\t"ID" : ' . $ID . ',\n';
+		echo '\t\t"Location" : \n' . $Location->JSONize() . ',\n';
+		echo '\t\t"Field" : ' . $Field->ID . ',\n';
+		echo '\t\t"ControlCost" : ' . $ControlCost . ',\n';
+		echo '\t\t"CropValue" : ' . $CropValue . ',\n';
+		echo '\t\t"AphidCount" : ' . $AphidCount . ',\n';
+		echo '\t\t"MummyCount" : ' . $MummyCount . ',\n';
+		echo '\t\t"Notes" : "' . $Notes . '",\n';
+		echo '\t\t"OtherPests" : [\n';
+		for($i = 0; $i < count($OtherPests); $i++)
+		{
+			echo '\t\t\t"' . $OtherPests[i] . '"';
+			
+			if($i < count($OtherPests) - 1)
+			{
+				echo ',\n';
+			}
+			else
+			{
+				echo '\n';
+			}
+		}
+		
+		echo '\t\t]\n';
+		echo '\t}';
+	}
+}
 
 ?>
