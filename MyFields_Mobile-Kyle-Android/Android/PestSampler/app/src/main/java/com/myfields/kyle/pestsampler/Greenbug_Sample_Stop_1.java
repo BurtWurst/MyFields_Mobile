b7db4bb861e5e98fa74e1ab94bb20852511325db
murphy52589 <kylemurphy52589@gmail.com>
2015-04-05 18:36:58 -0500

    small changes

--- MyFields_Mobile-Kyle-Android/Android/PestSampler/app/src/main/java/com/myfields/kyle/pestsampler/Greenbug_Sample_Stop_1.java
+++ MyFields_Mobile-Kyle-Android/Android/PestSampler/app/src/main/java/com/myfields/kyle/pestsampler/Greenbug_Sample_Stop_1.java
@@ -25,9 +25,9 @@ public class Greenbug_Sample_Stop_1 extends Activity{
     {
         textShown = (TextView)findViewById(R.id.greenbug_sample_stop1_help_shown);
         //hides the text until it's clicked
-        textShown.setVisibility(View.GONE);
+        //textShown.setVisibility(View.GONE);
     }
-    private void ToggleContents(View view)
+    /*private void ToggleContents(View view)
     {
         if(textShown.isShown()){
             //Fx.slide_up(this, textShown); STILL HAVE TO WRITE THE SLIDE UP PART IN CLASS FX AND THIS
@@ -37,5 +37,5 @@ public class Greenbug_Sample_Stop_1 extends Activity{
             textShown.setVisibility(View.VISIBLE);
             Fx.slide_down(this, textShown);
         }
-    }
+    }*/
 }
