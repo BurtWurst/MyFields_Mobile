d9250f4bab457854a56f39ac5d23bfb39eba3080
murphy52589 <kylemurphy52589@gmail.com>
2015-04-07 16:12:15 -0500

    Initial sample page done.
    Changes that need to be done are simply UI updates. Everything looks
    like shit tbh

--- MyFields_Mobile-Kyle-Android/Android/PestSampler/app/src/main/java/com/myfields/kyle/pestsampler/Greenbug_Sample_Stop_1.java
+++ MyFields_Mobile-Kyle-Android/Android/PestSampler/app/src/main/java/com/myfields/kyle/pestsampler/Greenbug_Sample_Stop_1.java
@@ -1,19 +1,27 @@
 package com.myfields.kyle.pestsampler;
 
 import android.app.Activity;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 /**
- First page on the greenbug sample stop
+ Greenbug sample stop
  */
-public class Greenbug_Sample_Stop_1 extends Activity{
+public class Greenbug_Sample_Stop_1 extends ActionBarActivity{
 
     private ListView listView;
+    private GridView gridView;
+    private GridViewAdapter gridViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +29,7 @@ public class Greenbug_Sample_Stop_1 extends Activity{
 
         setContentView(R.layout.activity_greenbug_sample_stop1);
         listView = (ListView) findViewById(R.id.greenbug_sample_page1);
+        gridView = (GridView)findViewById(R.id.greenbug_sample_stop1_gridview);
 
         CreateLayout();
     }
@@ -30,5 +39,18 @@ public class Greenbug_Sample_Stop_1 extends Activity{
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, android.R.id.text1, items);
         listView.setAdapter(adapter);
+
+        gridViewAdapter = new GridViewAdapter(this, R.layout.grid_item_layout, getData());
+        gridView.setAdapter(gridViewAdapter);
+    }
+
+    private ArrayList<ImageItem> getData() {
+        final ArrayList<ImageItem> imageItems = new ArrayList<>();
+        TypedArray imgs = getResources().obtainTypedArray(R.array.image_ids);
+        for (int i = 0; i < imgs.length(); i++) {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), imgs.getResourceId(i, -1));
+            imageItems.add(new ImageItem(bitmap, "Image#" + i));
+        }
+        return imageItems;
     }
 }
