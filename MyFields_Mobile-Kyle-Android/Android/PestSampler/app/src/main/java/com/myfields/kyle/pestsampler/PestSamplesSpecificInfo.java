bffb33558668e56abb8869beaf092130cb093b63
Daniel G. Kennedy <dgken2015@gmail.com>
2015-04-09 23:48:54 -0500

    Implemented the pest sample specific info page

--- MyFields_Mobile-Kyle-Android/Android/PestSampler/app/src/main/java/com/myfields/kyle/pestsampler/PestSamplesSpecificInfo.java
+++ MyFields_Mobile-Kyle-Android/Android/PestSampler/app/src/main/java/com/myfields/kyle/pestsampler/PestSamplesSpecificInfo.java
@@ -2,7 +2,7 @@ package com.myfields.kyle.pestsampler;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
+import android.text.TextUtils;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -16,9 +16,7 @@ public class PestSamplesSpecificInfo extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        View focusView = null;
-
-        setContentView(R.layout.activity_fields_list);
+        setContentView(R.layout.pest_samples_specific_info);
 
         listView = (ListView) findViewById(R.id.pest_samples_specific_info_list);
 
@@ -26,6 +24,45 @@ public class PestSamplesSpecificInfo extends Activity{
     }
     private void createListView()
     {
+        final String[] selectionList;
+        int fieldIndex = this.getIntent().getIntExtra("FieldIndex", 0);
+        int sampleIndex = this.getIntent().getIntExtra("SampleIndex", 0);
+        PestSample p = Globals.currentUser.getFields().get(fieldIndex).getPestSamples().get(sampleIndex);
 
+        if(p instanceof GreenbugSample)
+        {
+            GreenbugSample specificSample = (GreenbugSample) p;
+            selectionList = new String[10]; // Number of data items in a Greenbug Sample + sample type string
+
+            selectionList[0] = "Sample Type: \n\t" + "Greenbug Sample";
+            selectionList[1] = "Specific Sample ID: \n\t" + specificSample.getSpecificID();
+            selectionList[2] = "Generic Sample ID: \n\t" + specificSample.getID();
+            selectionList[3] = "Treatment Recommendation: \n\t" +
+                    (specificSample.getTreatmentRecommendation() ?
+                            "Treat Field for Greenbugs" :
+                            "Do Not Treat Field for Greenbugs");
+            selectionList[4] = "Aphid Count: \n\t" + specificSample.getAphidCount();
+            selectionList[5] = "Mummy Count: \n\t" + specificSample.getMummyCount();
+            selectionList[6] = "Crop Value: \n\t" + specificSample.getCropValue();
+            selectionList[7] = "Control Cost: \n\t" + specificSample.getControlCost();
+            selectionList[8] = "Notes: \n\t" + specificSample.getNotes();
+            selectionList[9] = "Other Pests: \n\t" + TextUtils.join("\n\t", specificSample.getOtherPests());
+        }
+        else
+        {
+            selectionList = new String[0]; // No data
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1, selectionList);
+        listView.setAdapter(adapter);
+
+    }
+
+    @Override
+    public void onStop()
+    {
+        super.onStop();
+
+        finish();
     }
 }
