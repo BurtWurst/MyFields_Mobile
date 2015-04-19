21349be4e7a2cd58f34d64bba408da66bf2f8cc1
murphy52589 <kylemurphy52589@gmail.com>
2015-04-19 18:48:58 -0500

    Comments and deleted a class that wasn't used

--- MyFields_Mobile-Kyle-Android/Android/PestSampler/app/src/main/java/com/myfields/kyle/pestsampler/FieldsList.java
+++ MyFields_Mobile-Kyle-Android/Android/PestSampler/app/src/main/java/com/myfields/kyle/pestsampler/FieldsList.java
@@ -10,12 +10,29 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 
-/**
- Represents the fields & names along with the type of field it is (corn, wheat, etc...)
- */
-public class FieldsList extends Activity {
-    private ListView listView;
 
+// ***************************************************************
+// * OVERVIEW                                                    *
+// * This activity class represents the Fields List page, which  *
+// * shows the fields & names along with what type of field it is*
+// ***************************************************************
+
+public class FieldsList extends Activity {
+    private ListView listView; //listview used to create the layout
+
+    // ***************************************************************
+    // * OVERVIEW                                                    *
+    // * ----------------------------------------------------------- *
+    // * This function creates the activity for the Fields List page.*
+    // * It calls the createListView method which does the work      *
+    // ***************************************************************
+    // * PARAMETERS                                                  *
+    // * ----------------------------------------------------------- *
+    // * savedInstanceState                                          *
+    // *    This parameter specifies any data that was saved by a    *
+    // *    previous run of the activity; i.e. the last time that    *
+    // *    onSaveInstanceState was called; otherwise null.          *
+    // ***************************************************************
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,11 +44,18 @@ public class FieldsList extends Activity {
         createListView();
     }
 
+    // ***************************************************************
+    // * OVERVIEW                                                    *
+    // * This function creates the activity, which is a list view    *
+    // * that loads in field info from the api then puts it into     *
+    // * the api                                                     *
+    // ***************************************************************
     private void createListView() {
 
-        ArrayList<Field> fieldsList = Globals.currentUser.getFields();
-        final String[] selectionList = new String[fieldsList.size()];
+        ArrayList<Field> fieldsList = Globals.currentUser.getFields(); //gets the fieldsList from the current user
+        final String[] selectionList = new String[fieldsList.size()]; //gets the size of the field list
 
+        //puts the fields list info into selectionList
         for (int i = 0; i < fieldsList.size(); i++) {
             selectionList[i] = fieldsList.get(i).Name;
         }
