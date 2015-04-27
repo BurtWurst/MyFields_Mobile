e936525fa9b48944ef1e7af2a1533b6c6936f8b7
murphy52589 <kylemurphy52589@gmail.com>
2015-04-27 17:00:19 -0500

    Commented code for 4 classes

--- MyFields_Mobile-Kyle-Android/Android/PestSampler/app/src/main/java/com/myfields/kyle/pestsampler/PlantingsSpecificInfo.java
+++ MyFields_Mobile-Kyle-Android/Android/PestSampler/app/src/main/java/com/myfields/kyle/pestsampler/PlantingsSpecificInfo.java
@@ -9,9 +9,7 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
-/**
- * Created by Kyle on 4/9/2015.
- */
+//class that represents the plantings specific info activity page
 public class PlantingsSpecificInfo extends Activity {
     private ListView listView;
     private Button home_Button;
@@ -22,22 +20,25 @@ public class PlantingsSpecificInfo extends Activity {
 
         setContentView(R.layout.plantings_specific_info);
 
-        home_Button = (Button) findViewById(R.id.plantings_specific_info_home_button);
-        listView = (ListView) findViewById(R.id.plantings_specific_info_list);
+        home_Button = (Button) findViewById(R.id.plantings_specific_info_home_button); //sets the button declared earlier to the button from the xml file
+        listView = (ListView) findViewById(R.id.plantings_specific_info_list); //sets the listview to the one created in the xml
 
+		//header to show above the listview with the text "complete plantings info"
         TextView header = new TextView(this);
         header.setText("Complete Plantings Info: ");
         listView.addHeaderView(header);
 
-        createListView();
+        createListView(); //calls the method 2 lines below that creates the layout
     }
     private void createListView()
     {
         final String[] selectionList = new String[6]; // Number of data items in a Planting
-        int fieldIndex = this.getIntent().getIntExtra("FieldIndex", 0);
-        int plantingIndex = this.getIntent().getIntExtra("PlantingIndex", 0);
+        int fieldIndex = this.getIntent().getIntExtra("FieldIndex", 0); //retrieves the extended data field index from the intent
+        int plantingIndex = this.getIntent().getIntExtra("PlantingIndex", 0); //retrieves the extended data plantings index from the intent
         Planting p = Globals.currentUser.getFields().get(fieldIndex).getPlantingList().get(plantingIndex);
+		//above line gets the fields and the plantings list based on the two indexes
 
+		//on the home button's click, takes the user back to the selection screen
         home_Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -46,16 +47,16 @@ public class PlantingsSpecificInfo extends Activity {
             }
         });
 
-        selectionList[0] = "ID: \n\t" + p.getID();
-        selectionList[1] = "Crop Type: \n\t" + p.getCropType();
-        selectionList[2] = "Crop Variety: \n\t" + p.getCropVariety();
-        selectionList[3] = "Crop Density (thousand seeds/acre): \n\t" + p.getCropDensity();
-        selectionList[4] = "Date Of Planting: \n\t" + p.getDateOfPlanting();
-        selectionList[5] = "Notes: \n\t" + p.getNotes();
+        selectionList[0] = "ID: \n\t" + p.getID(); //first item in the listview is the planting id
+        selectionList[1] = "Crop Type: \n\t" + p.getCropType(); //second item in the listview is the crop type
+        selectionList[2] = "Crop Variety: \n\t" + p.getCropVariety(); //third item in the listview is the crop variety
+        selectionList[3] = "Crop Density (thousand seeds/acre): \n\t" + p.getCropDensity(); //fourth item in the listview is the crop density
+        selectionList[4] = "Date Of Planting: \n\t" + p.getDateOfPlanting(); //fifth item in the listview is the date of the planting
+        selectionList[5] = "Notes: \n\t" + p.getNotes(); //sixth item in the listview are the notes about a planting
 
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, android.R.id.text1, selectionList);
-        listView.setAdapter(adapter);
+        listView.setAdapter(adapter); //creates the adapter and sets it based on the info above
 
     }
 
@@ -64,6 +65,6 @@ public class PlantingsSpecificInfo extends Activity {
     {
         super.onStop();
 
-        finish();
+        finish(); //goes back to the last activity safely
     }
 }
